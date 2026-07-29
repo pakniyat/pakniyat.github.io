f99c2406323e064ed69001a51dd7a5088b97f90c
pakniyat <38963955+pakniyat@users.noreply.github.com>
2026-07-28 21:23:00 -0700

    Add files via upload

--- branding.js
+++ branding.js
@@ -1,7 +1,6 @@
 function aliheader(){
-	document.write('<img src="UA_Reverse.png" vspace="10"/>');
+    document.write('<img src="M3AC_Logo_Horizontal.png" alt="Multi-Modal Multi-Agent Control (M3AC) Lab" onerror="this.style.display=\'none\'" />');
 }
 
 function alifooter(){
-	
 }
